9e5e5c6f361e52a74629504cb32f80ef491b1c47
sandra <sandra.rum@inqbus.de>
2022-03-22 16:17:44 +0100

    Dynamic Page added

---
+++ pages/[dataset].vue
@@ -0,0 +1,16 @@
+<template>
+    <NuxtLayout name="plone">
+        <h1>{{ $route.params.dataset }}</h1>
+        <p>Das ist eine dynamischer Seite</p>
+    </NuxtLayout>
+</template>
+
+<script>
+    export default {
+        name: "[dataset].vue",
+    }
+</script>
+
+<style scoped>
+
+</style>

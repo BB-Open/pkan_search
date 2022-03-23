42e452e45eeff334d40eebeb57535c0e6cd394ff
sandra <sandra.rum@inqbus.de>
2022-03-23 10:49:27 +0100

    Blog as dynamic page

---
+++ pages/blog/blog-[id].vue
@@ -0,0 +1,16 @@
+<template>
+    <NuxtLayout name="plone">
+        <h1>{{ $route.params.id }}</h1>
+        <p>Das ist eine dynamischer Seite</p>
+    </NuxtLayout>
+</template>
+
+<script>
+    export default {
+        name: "blog-[id].vue",
+    }
+</script>
+
+<style scoped>
+
+</style>

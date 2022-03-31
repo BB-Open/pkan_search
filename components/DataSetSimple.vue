3a5c54f088c4f495a28ddd28e1c1d5aefb803ccf
sandra <sandra.rum@inqbus.de>
2022-03-31 14:09:35 +0200

    Handle not existing fields

--- components/DataSetSimple.vue
+++ components/DataSetSimple.vue
@@ -1,9 +1,14 @@
 <template>
     <div class='testtest'>
-      <NuxtLink :to="'/' + encodeURIComponent(item.id)" :aria-label="item.dcterms_title+ ' weiterlesen'">
+      <NuxtLink v-if="item.dcterms_title" :to="'/' + encodeURIComponent(item.id)" :aria-label="item.dcterms_title[0] + ' weiterlesen'">
         <h2 class="element_title">{{ item.dcterms_title[0] }}</h2>
       </NuxtLink>
-      <p class="element_description">{{ item.dcterms_description[0].split('.')[0] }}. ...</p>
+        <NuxtLink v-if="!item.dcterms_title" :to="'/' + encodeURIComponent(item.id)"
+                  :aria-label="item.id + ' weiterlesen'">
+            <h2 class="element_title">{{ item.id }}</h2>
+        </NuxtLink>
+      <p class="element_description" v-if="item.dcterms_description">{{ item.dcterms_description[0].split('.')[0] }}. ...</p>
+      <p class="element_description" v-if="!item.dcterms_description">Keine Beschreibung verfügbar</p>
       <Distributions :distributions="JSON.parse(item.dcat_distribution)" >
       </Distributions>
     </div>

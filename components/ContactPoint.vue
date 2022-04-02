2646b51e9c5a3fc51e596e84df67b6c672254d8d
volker <volker.jaenisch@inqbus.de>
2022-04-02 19:52:53 +0200

    More contact details

--- components/ContactPoint.vue
+++ components/ContactPoint.vue
@@ -1,12 +1,17 @@
 <template>
   <div>
   <h3>Kontakt:</h3>
-  <div class="contact street_address" v-if="contactPoint['vcard_street-address']">{{contactPoint['vcard_street-address']}}</div>
-  <div class="contact_postal" v-if="contactPoint['vcard_postal-code']">{{contactPoint['vcard_postal-code']}}</div>
-  <div class="contact locality"  v-if="contactPoint['vcard_locality']">{{contactPoint.vcard_locality}} </div>
-  <div class="contact region"  v-if="contactPoint.vcard_region">{{contactPoint.vcard_region}}</div>
-  <div class="contact country"  v-if="contactPoint['vcard_country-name']">{{contactPoint['vcard_country-name']}}</div>
-  <div class="contact email" v-if="contactPoint.vcard_email">{{contactPoint.vcard_email}}</div>
+  <div class="contact organization" v-if="contactPoint['vcard_organization']">{{contactPoint['vcard_organization']}}</div>
+  <div class="contact_street_address" v-if="contactPoint['vcard_street-address']">{{contactPoint['vcard_street-address']}}</div>
+  <div>
+    <span class="contact_postal" v-if="contactPoint['vcard_postal-code']">{{contactPoint['vcard_postal-code']}}</span>
+    <span class="contact_locality"  v-if="contactPoint['vcard_locality']"> {{contactPoint.vcard_locality}} </span>
+  </div>
+<!--  <div class="contact_region"  v-if="contactPoint.vcard_region">{{contactPoint.vcard_region}}</div> -->
+  <div class="contact_country"  v-if="contactPoint['vcard_country-name']">{{contactPoint['vcard_country-name']}}</div>
+  <div class="contact_email" v-if="contactPoint.vcard_email">{{contactPoint.vcard_email}}</div>
+  <div class="contact_voice" v-if="contactPoint.vcard_voice">Tel. {{contactPoint.vcard_voice}}</div>
+  <div class="contact_fax" v-if="contactPoint.vcard_fax">Fax {{contactPoint.vcard_fax}}</div>
   </div>
 </template>
 

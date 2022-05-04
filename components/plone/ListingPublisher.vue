0f0ddec3928be17bd0763510f58f1bb9865e915f
sandra <sandra.rum@inqbus.de>
2022-05-04 16:12:52 +0200

    Blackborder for inputs and selects Listing Publisher new Styling

--- components/plone/ListingPublisher.vue
+++ components/plone/ListingPublisher.vue
@@ -1,15 +1,13 @@
 <template>
     <div>
-        <ul class="box_area nobull">
+        <ul class="box_area nobull category">
             <li v-for="item in ploneStore.ploneListing['PublisherCard']" class="box" v-if="ploneStore.ploneListing['PublisherCard'] && ploneStore.ploneListing['PublisherCard'].length">
-              <NuxtLink :to="'/publisher/publisher-' + item.UID" :aria-label="item.title + ' weiterlesen'">
-                <div class="plone_listing_element">
-                    <h2 class="element_title">{{ item.title }}</h2>
-                    <div class="element_date" v-if="item.date_text">{{ item.date_text }}</div>
-                    <div class="element_logo" v-if="item.logo"><img :src="item.logo.download" :alt="item.title + ' Logo'"/></div>
-                    <div class="element_description">{{ item.description }}</div>
-                </div>
-              </NuxtLink>
+                  <NuxtLink :to="'/publisher/publisher-' + item.UID" :aria-label="item.title + ' weiterlesen'" class="plone_listing_element lightbutton button vocabbutton">
+                        <div class="element_title_box">
+                        <h2 class="element_title">{{ item.title }}</h2>
+                        </div>
+                        <div class="element_logo" v-if="item.logo"><img :src="item.logo.download" :alt="item.title + ' Logo'"/></div>
+                  </NuxtLink>
             </li>
         </ul>
         <div v-if="!ploneStore.ploneListing['PublisherCard'] || !ploneStore.ploneListing['PublisherCard'].length">
@@ -38,6 +36,40 @@
 </script>
 
 <style scoped>
+    ul {
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        justify-content: space-between;
+    }
+
+    .element_title_box {
+        margin-bottom: 15px;
+    }
+
+    .vocabbutton {
+        width: 100%;
+        height: 100%;
+        padding: 15px;
+    }
+
+    .vocabbutton {
+        text-decoration: none;
+        display: block;
+        height: 100%;
+        width: 100%;
+    }
+
+    .vocabbutton:hover, .vocabbutton:focus {
+        background-color: #C13B33;
+        color: #fff;
+    }
+
+    h2 {
+        color: inherit;
+    }
+
+
     .element_title {
         font-weight: bold;
         font-style: normal;
@@ -47,10 +79,7 @@
 
     .element_logo img {
         width: 240px;
-    }
-
-    .element_date {
-        font-style: italic;
+        background-color: #eeeeee;
     }
 
     .plone_listing_element {

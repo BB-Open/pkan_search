7fdd827a8b028e53cc19e1eb4fe34fc437e663a2
volker <volker.jaenisch@inqbus.de>
2022-05-06 22:09:50 +0200

    Deeplink functionality

---
+++ components/URL.vue
@@ -0,0 +1,25 @@
+<template>
+  <div></div>
+</template>
+
+<script setup lang='ts'>
+  import {useEntityStore} from '~/stores/entities'
+
+  const entityStore = useEntityStore();
+  const router = useRouter();
+
+    watch( [entityStore.showDeepLinks], (new_val, old_val) => {
+      let currentRoute = router.currentRoute
+      if (entityStore.showDeepLinks) {
+        router.replace(currentRoute.value.path + '#/' + entityStore.getParams)
+      } else
+      {
+        router.replace(currentRoute.value.path)
+      }
+    })
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ components/deep.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="deep">
+    <h2 class="hidden_help_text">Suchlink Anzeigen</h2>
+      <div class="input_checkbox">
+        <input type='checkbox'
+           name='Suchlink anzeigen'
+           :checked='entityStore.showDeepLinks'
+           @click='entityStore.toggleDeepLinks'
+           @keydown.enter.prevent="entityStore.toggleDeepLinks">
+        <label>Suchlink anzeigen</label>
+      </div>
+  </div>
+</template>
+
+<script setup lang="ts" >
+  import {useEntityStore} from '~/stores/entities'
+
+  const entityStore = useEntityStore()
+
+</script>
+
+<style scoped>
+
+</style>

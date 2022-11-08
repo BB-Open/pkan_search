4170076813480caebebcd43fca2e51afc71b98b2
sandra <sandra.rum@inqbus.de>
2022-11-08 08:50:19 +0100

    Removed all details except title and description

--- components/DatasetRoulette.vue
+++ components/DatasetRoulette.vue
@@ -8,29 +8,29 @@
       <div>
         <p class="dct_description" v-if='entityStore.roulette.dct_description'>{{ entityStore.roulette.dct_description[0] }}</p>
       </div>
-      <div v-if="entityStore.roulette.dcat_distribution">
-        <h4>Veröffentlichungen:</h4>
-        <Distributions :distributions="JSON.parse(entityStore.roulette.dcat_distribution)" >
-        </Distributions>
-      </div>
-      <div v-if="entityStore.roulette.dcat_endpointURL">
-        <h4>Service URIs:</h4>
-        <Endpoints v-if='entityStore.roulette.dcat_endpointURL' :endpoints="JSON.parse(entityStore.roulette.dcat_endpointURL)" >
-        </Endpoints>
-      </div>
-      <div  v-if="entityStore.roulette.dcat_servesDataset">
-        <h4>Angebotene Datensätze:</h4>
-        <Datasets v-if='entityStore.roulette.dcat_servesDataset' :datasets="JSON.parse(entityStore.roulette.dcat_servesDataset)" >
-        </Datasets>
-      </div>
-      <div v-if='entityStore.roulette.dct_license_facet'>
-        <h4>Lizenz:</h4>
-        <div class="lizenz">{{entityStore.roulette.dct_license_facet}}</div>
-      </div>
-      <div v-if="entityStore.roulette.dct_rightsstatement">
-        <h4>Rechtliche Aussage:</h4>
-        <div class="statement">{{entityStore.roulette.dct_rightsstatement[0]}}</div>
-      </div>
+<!--      <div v-if="entityStore.roulette.dcat_distribution">-->
+<!--        <h4>Veröffentlichungen:</h4>-->
+<!--        <Distributions :distributions="JSON.parse(entityStore.roulette.dcat_distribution)" >-->
+<!--        </Distributions>-->
+<!--      </div>-->
+<!--      <div v-if="entityStore.roulette.dcat_endpointURL">-->
+<!--        <h4>Service URIs:</h4>-->
+<!--        <Endpoints v-if='entityStore.roulette.dcat_endpointURL' :endpoints="JSON.parse(entityStore.roulette.dcat_endpointURL)" >-->
+<!--        </Endpoints>-->
+<!--      </div>-->
+<!--      <div  v-if="entityStore.roulette.dcat_servesDataset">-->
+<!--        <h4>Angebotene Datensätze:</h4>-->
+<!--        <Datasets v-if='entityStore.roulette.dcat_servesDataset' :datasets="JSON.parse(entityStore.roulette.dcat_servesDataset)" >-->
+<!--        </Datasets>-->
+<!--      </div>-->
+<!--      <div v-if='entityStore.roulette.dct_license_facet'>-->
+<!--        <h4>Lizenz:</h4>-->
+<!--        <div class="lizenz">{{entityStore.roulette.dct_license_facet}}</div>-->
+<!--      </div>-->
+<!--      <div v-if="entityStore.roulette.dct_rightsstatement">-->
+<!--        <h4>Rechtliche Aussage:</h4>-->
+<!--        <div class="statement">{{entityStore.roulette.dct_rightsstatement[0]}}</div>-->
+<!--      </div>-->
     </div>
     <div v-else class="dataset">
       Der Datensatz wird noch geladen.

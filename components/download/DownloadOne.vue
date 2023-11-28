cffe45949522ffed867989ba86116ec4dffa5bd8
sandra <sandra.rum@inqbus.de>
2023-11-28 10:55:25 +0100

    Implemented DownloadOne.vue

--- components/download/DownloadOne.vue
+++ components/download/DownloadOne.vue
@@ -1,15 +1,18 @@
 <template>
   <h3>Metadaten Download:</h3>
   <ul>
-    <li>RDF/XML exportieren: <a>Metadaten herunterladen</a></li>
-    <li>RDF/JSON exportieren: <a>Metadaten herunterladen</a></li>
-    <li>RDF/TTL exportieren: <a>Metadaten herunterladen</a></li>
+    <li>RDF/XML exportieren: <a :href="DOWNLOAD_ONE + '?id=' + encodeURIComponent(entityStore.dataset_uri)">Metadaten herunterladen</a></li>
+    <li>RDF/JSON exportieren: <a :href="DOWNLOAD_ONE + '?format=rdf/json&id=' + encodeURIComponent(entityStore.dataset_uri)">Metadaten herunterladen</a></li>
+    <li>RDF/TTL exportieren: <a :href="DOWNLOAD_ONE + '?format=rdf/ttl&id=' + encodeURIComponent(entityStore.dataset_uri)">Metadaten herunterladen</a></li>
   </ul>
 </template>
 
 
 <script setup lang="ts">
+import {DOWNLOAD_ONE} from "/etc/pkan/nuxt_config";
+import {useEntityStore} from "~/stores/entities";
 
+const entityStore = useEntityStore()
 </script>
 
 
